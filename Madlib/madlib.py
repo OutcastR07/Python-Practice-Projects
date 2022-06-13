2f87291e78bea2eb651b86113c8ba4935ad3f6c0
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2022-06-13 20:20:57 +0600

    Madlib

---
+++ Madlib/madlib.py
@@ -0,0 +1,13 @@
+a = input("1) ")
+b = input("2) ")
+c = input("3) ")
+d = input("4) ")
+e = input("5) ")
+
+
+madlib = f"Regular physical {a} can relieve stress, anxiety, depression and anger. \
+Most people {b} they feel better over time as physical activity becomes a {c} part of their lives. \
+Without regular {d}, your body slowly loses its strength, stamina and ability to function properly. \
+Exercise increases muscle strength, which in turn {e} your ability to do other physical activities."
+
+print(madlib)
